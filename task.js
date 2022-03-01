ec8397d5034755610828f44535804cc5a90914b5
ninoxin <75312516+ninoxin@users.noreply.github.com>
2022-03-01 16:01:53 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,17 +1,30 @@
-const numbers = [1, 2, 3, 4, 5, 6];
+const form = document.forms.formWithInput;
+const printResult = document.querySelector('.content__result');
+const inputNumber = document.querySelector('.form__input');
+const congratulation = document.querySelector('#congratulation');
 
-separateArray(numbers);
-// должен вернуть { even: [2, 4, 6], odd: [1, 3, 5] }
-
-function separateArray(array) {
-    let even = [], odd = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % 2 == 0) {
-            even.push(array[i]);
-        } else {
-            odd.push(array[i])
-        }
-    }
-    console.log('even ' + '[' + even + ']');
-    console.log('odd ' + '[' + odd + ']');
+function findNearestFactorial(value) {
+  if (value === '' || value === undefined) {
+    return '*';
+  }
+  if (value <= 0) {
+    console.log(value);
+    return 1;
+  }
+  let currentValue = 1;
+  let i = 1
+  for (let i = 1; currentValue * i <= value; i++) {
+    currentValue *= i;
+  }
+  return i - 1;
 }
+
+form.addEventListener('keyup', evt => {
+  evt.preventDefault();
+  printResult.textContent = findNearestFactorial(inputNumber.value);
+  congratulation.textContent = inputNumber.value && 'Вау, это успех!';
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+});
