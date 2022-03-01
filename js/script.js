788cc740b1331644be510ffbcf833754b8104237
ninoxin <75312516+ninoxin@users.noreply.github.com>
2022-03-01 16:05:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,17 +7,22 @@ const guestsInfo = [
 ]
 
 
-let ageSum = 0;
-const len = guestsInfo.length;
-let item = null;
-for (let i = 0; i < len; i++) {
-    item = guestsInfo[i];
-    ageSum = item.age + ageSum;
-        
-}
-const averageage = ageSum / len;
-console.log("Average age:", averageage);
 
+function calcAvgAge(array) {
+  let ageSum = 0;
+  const len = guestsInfo.length;
+  let item = null;
+  for (let i = 0; i < len; i++) {
+      item = guestsInfo[i];
+      ageSum = item.age + ageSum;
+          
+  }
+  const averageage = ageSum / len;
+  console.log("Average age:", averageage);
+}
+
+
+calcAvgAge(guestsInfo)
 
 
 
