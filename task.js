0af07f5c6212dd0ee1b816c1c57a4b2bcc96822b
ninoxin <75312516+ninoxin@users.noreply.github.com>
2022-03-01 16:01:04 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,10 +1,17 @@
-var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
+const numbers = [1, 2, 3, 4, 5, 6];
 
-getLastElementOfArray(words); // возвращает Свет;
+separateArray(numbers);
+// должен вернуть { even: [2, 4, 6], odd: [1, 3, 5] }
 
-function getLastElementOfArray(array) {
-  let last = array[words.length - 1]; 
-  return last
+function separateArray(array) {
+    let even = [], odd = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            even.push(array[i]);
+        } else {
+            odd.push(array[i])
+        }
+    }
+    console.log('even ' + '[' + even + ']');
+    console.log('odd ' + '[' + odd + ']');
 }
-
-console.log(getLastElementOfArray(words));
