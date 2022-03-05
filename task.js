6bde9d22ff2b4a9f0b3956932bda72a161956692
ninoxin <75312516+ninoxin@users.noreply.github.com>
2022-03-05 13:16:01 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,5 +1,31 @@
 function getDomNodesBySelector(selector) {
-    return Array.from(document.querySelectorAll(selector))
-}
+
+  return Array.from(document.querySelectorAll(selector));
+  }
+
+  let count = 0
+  document.querySelector('.total__button').addEventListener('click', () =>{
+    if (count == 0) {
+      applyDiscount()
+      count++
+    }
+  });
+
+function applyDiscount() {
   
-  console.log(getDomNodesBySelector('.price-value'));
+  let array = getDomNodesBySelector('.price-value')
+  let totalPrice = document.querySelector('.total-price-value')
+
+  let sum = 0;
+
+  for(let i = 0; i < array.length; i++){
+    sum = +array[i].textContent * 0.15;
+    array[i].textContent -= sum
+  }
+
+  array.forEach(elem => {
+    sum += +elem.textContent 
+    
+  })
+  totalPrice.textContent = sum
+}
